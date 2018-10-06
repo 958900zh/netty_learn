4d046bfd6b0420b32e82adfd173482f600560fa4
958900zh <zxdong_55@163.com>
2018-10-06 23:51:14 +0800

    ProtoBuf多协议消息

--- src/main/java/com/demo/netty/sixth/TestClientHandler.java
+++ src/main/java/com/demo/netty/sixth/TestClientHandler.java
@@ -3,18 +3,44 @@ package com.demo.netty.sixth;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 
-public class TestClientHandler extends SimpleChannelInboundHandler<DataInfo.Student> {
+import java.util.Random;
+
+public class TestClientHandler extends SimpleChannelInboundHandler<DataInfo.MyMessage> {
 
     @Override
-    protected void channelRead0(ChannelHandlerContext ctx, DataInfo.Student msg) throws Exception {
+    protected void channelRead0(ChannelHandlerContext ctx, DataInfo.MyMessage msg) throws Exception {
 
     }
 
     @Override
     public void channelActive(ChannelHandlerContext ctx) throws Exception {
-        DataInfo.Student student = DataInfo.Student.newBuilder()
-                .setName("张三").setAge(20).setAddress("北京").build();
 
-        ctx.writeAndFlush(student);
+        int i = new Random().nextInt(3);
+
+        DataInfo.MyMessage message = null;
+
+        if (i == 0) {
+            message = DataInfo.MyMessage.newBuilder()
+                    .setDataType(DataInfo.MyMessage.DataType.StudentType)
+                    .setStudent(
+                            DataInfo.Student.newBuilder()
+                                    .setName("张三").setAge(20).setAddress("北京")
+                                    .build()
+                    ).build();
+        } else if (i == 1) {
+            message = DataInfo.MyMessage.newBuilder()
+                    .setDataType(DataInfo.MyMessage.DataType.TeacherType)
+                    .setTeacher(
+                            DataInfo.Teacher.newBuilder().setName("李四").setAge(35).build()
+                    ).build();
+        } else {
+            message = DataInfo.MyMessage.newBuilder()
+                    .setDataType(DataInfo.MyMessage.DataType.DogType)
+                    .setDog(
+                            DataInfo.Dog.newBuilder().setName("哈士奇").build()
+                    ).build();
+        }
+
+        ctx.writeAndFlush(message);
     }
 }
--- src/main/java/com/demo/netty/sixth/TestServerHandler.java
+++ src/main/java/com/demo/netty/sixth/TestServerHandler.java
@@ -3,12 +3,21 @@ package com.demo.netty.sixth;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 
-public class TestServerHandler extends SimpleChannelInboundHandler<DataInfo.Student> {
+public class TestServerHandler extends SimpleChannelInboundHandler<DataInfo.MyMessage> {
 
     @Override
-    protected void channelRead0(ChannelHandlerContext ctx, DataInfo.Student msg) throws Exception {
-        System.out.println("来自客户端: " + msg.getName());
-        System.out.println("来自客户端: " + msg.getAge());
-        System.out.println("来自客户端: " + msg.getAddress());
+    protected void channelRead0(ChannelHandlerContext ctx, DataInfo.MyMessage msg) throws Exception {
+        DataInfo.MyMessage.DataType dataType = msg.getDataType();
+        if (dataType == DataInfo.MyMessage.DataType.StudentType) {
+            System.out.println("来自客户端[student]: " + msg.getStudent().getName());
+            System.out.println("来自客户端[student]: " + msg.getStudent().getAge());
+            System.out.println("来自客户端[student]: " + msg.getStudent().getAddress());
+        } else if (dataType == DataInfo.MyMessage.DataType.TeacherType) {
+            System.out.println("来自客户端[teacher]: " + msg.getTeacher().getName());
+            System.out.println("来自客户端[teacher]: " + msg.getTeacher().getAge());
+        } else {
+            System.out.println("来自客户端[dog]: " + msg.getDog().getName());
+        }
+
     }
 }
